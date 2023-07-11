9a848f5507fcdb332c280a1493b6225f43a83911
Lucien Lin <luciensday@gmail.com>
2023-07-11 17:14:20 +0100

    var carchose

--- app.js
+++ app.js
@@ -14,24 +14,5 @@ Class Button{
 }
 
 
-const gridDisplay = document.querySelector("#grid")
-const resultDisplay = document.querySelector('#result')
-let cardsChosen = []
-let cardsChosenIds = []
-const cardsWon= []
-
-function createBoard(){
-    for (let i = 0; i < cardArray.length; i++ ) {
-        const card = document.createElement('img')
-        card.setAttribute('src', 'images/blank.png')
-        card.setAttribute('data-id', i )
-        card.addEventListener('click', flipCard)
-        gridDisplay.appendChild(card)
-
-    }
-
-
-function checkMatch() {
-    const cards = document.querySelectorAll('img')
-    const optionOneId = cardsChosenIds[0]
-    const optionTwoId = cardsChosenIds[1]
+cardsChosen = []
+var cardchosen = 30 
