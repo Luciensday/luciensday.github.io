fd6195704357ccfbcdb64c2deb8a40f9ee927a93
Lucien Lin <luciensday@gmail.com>
2023-07-12 15:55:18 +0100

    image loading

--- app.js
+++ app.js
@@ -49,3 +49,76 @@ function flipCard() {
         setTimeout(checkMatch, 500)
     }
 }
+
+
+const cardArray = [
+    {
+        name: 'fries',
+        img:'images/fries.png',
+    }, 
+    {
+        name: 'cheeseburger',
+        img:'images/cheeseburger.png',
+
+    }, 
+    {
+        name: 'hotdog',
+        img:'images/hotdog.png',
+
+
+    }, 
+    {
+        name: 'ice-cream',
+        img:'images/ice-cream.png',
+
+
+    }, 
+    {
+        name: 'milkshake',
+        img:'images/milkshake.png',
+
+
+    }, 
+    {
+        name: 'pizza',
+        img:'images/pizza.png',
+
+
+    }, 
+    {
+        name: 'fries',
+        img:'images/fries.png',
+
+    }, 
+    {
+        name: 'cheeseburger',
+        img:'images/cheeseburger.png',
+
+    }, 
+    {
+        name: 'hotdog',
+        img:'images/hotdog.png',
+
+
+    }, 
+    {
+        name: 'ice-cream',
+        img:'images/ice-cream.png',
+
+
+    }, 
+    {
+        name: 'milkshake',
+        img:'images/milkshake.png',
+
+
+    }, 
+    {
+        name: 'pizza',
+        img:'images/pizza.png',
+
+
+    }, 
+
+]
+
