d1613032c44a2fa10a32b14a7afbf7171418d937
Lucien Lin <luciensday@gmail.com>
2023-07-15 23:42:08 +0100

    fix some bug for toggle

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar_menu')
+const menuLinks = document.querySelector('.navbar-menu')
 
+
+//Display Mobile menu 
+
+const mobileMenu = () => {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+}
+
+menu.addEventListener('click', mobileMenu); 
